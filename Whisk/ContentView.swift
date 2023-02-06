ef472c0dddb9b7a34783401b1fb7a5e8773882a9
Nedine <nedinerafid@gmail.com>
2023-02-06 20:32:29 +0300

    Initial Commit

---
+++ Whisk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whisk
+//
+//  Created by Nedine on 2/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whisk/WhiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiskApp.swift
+//  Whisk
+//
+//  Created by Nedine on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WhiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
